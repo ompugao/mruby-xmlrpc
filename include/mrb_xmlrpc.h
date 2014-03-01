ae3e89094ef08a676930efbdf714dd5b7db7d403
Yoshihiko Yamanaka <bamchoh@gmail.com>
2014-03-01 23:09:51 +0900

    support mrb_intern_lit

--- include/mrb_xmlrpc.h
+++ include/mrb_xmlrpc.h
@@ -3,6 +3,6 @@
 
 #define MRUBY_XMLRPC_NAME "mruby xmlrpc client"
 #define MRUBY_XMLRPC_VERSION "0.0.1"
-#define E_XMLRPC_FAULTEXCEPTION  (mrb_class_ptr(mrb_const_get(mrb,mrb_obj_value(mrb_class_get(mrb,"XMLRPC")),mrb_intern(mrb,"FaultException"))))
+#define E_XMLRPC_FAULTEXCEPTION  (mrb_class_ptr(mrb_const_get(mrb,mrb_obj_value(mrb_class_get(mrb,"XMLRPC")),mrb_intern_lit(mrb,"FaultException"))))
 
 #endif
